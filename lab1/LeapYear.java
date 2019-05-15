93af72f887045800608a3cd910bddb09357d879a
Tian NIE <true.nie@gmail.com>
2019-05-15 23:26:52 +0200

    added isLeapYear method

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,10 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Tian NIE
  */
 public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
