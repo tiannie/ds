b4b56a43e85792712cdc2a2c34730b6d8fbcfd9a
Tian NIE <true.nie@gmail.com>
2019-05-19 21:56:44 +0200

    added file with placeholders for methods

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,13 @@
+public class LinkedListDeque<T> {
+    public void addFirst(T item) {}
+    public LinkedListDeque() {}
+    public LinkedListDeque(LinkedListDeque other) {}
+    public void addLast(T item) {}
+    public boolean isEmpty() {}
+    public int size() {}
+    public void printDeque() {}
+    public T removeFirst() {}
+    public T removeLast() {}
+    public T get(int index) {}
+    public T getRecursive(int index) {}
+}
