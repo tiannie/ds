af64d1bc8dfa10f7ef6de50042a2762b6bdaf376
Tian NIE <true.nie@gmail.com>
2019-06-01 01:47:05 +0200

    fixed api error (private access)

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,5 +1,5 @@
 public class OffByN implements CharacterComparator {
-    int offset;
+    private int offset;
     @Override
     public boolean equalChars(char x, char y) {
         return x - y == offset || x - y == -offset;
