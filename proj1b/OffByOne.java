735373e3157c56920b8ebf3c2a5b0eccf762a9fd
Tian NIE <true.nie@gmail.com>
2019-06-01 01:44:32 +0200

    fixed style errors

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
-    public boolean equalChars(char x, char y){
+    public boolean equalChars(char x, char y) {
         if (x - y == 1 || x - y == -1) {
             return true;
         }
